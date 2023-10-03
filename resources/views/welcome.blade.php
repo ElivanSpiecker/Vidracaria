8de0226ead0c2caa518fa8a673b26c9eb5a8935c
ElivanSpiecker <elivan.spiecker@universo.univates.br>
2023-10-02 22:24:29 -0300

    tela inicial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,6 +26,7 @@
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 font-semibold text-xl text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Registrar-se</a>
+                            <p>teste</p>
                         @endif
                     @endauth
                 </div>
